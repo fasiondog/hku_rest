470a23217a63b69aa9b57b2f14481f2455ec1576
fasiondog <fasiondog@163.com>
2026-03-28 04:19:59 +0800

    fix(xmake.lua): 修正hku_utils版本依赖配置
    将hku_utils的版本依赖从">=1.3.4"修改为">1.3.4"，
    以确保使用正确的版本范围配置。

--- xmake.lua
+++ xmake.lua
@@ -34,7 +34,7 @@ if get_config("use_hikyuu") then
         stacktrace = has_config("stacktrace")
     }})
 else
-    add_requires("hku_utils >=1.3.4", 
+    add_requires("hku_utils >1.3.4", 
         {configs = {
             shared = is_kind("shared"), 
             log_level = log_level,
