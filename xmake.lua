f028e1f1d904e786fd6a9d036cf7ef4a4b465854
fasiondog <fasiondog@163.com>
2026-04-12 03:38:02 +0800

    1.1.5

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_xmakever("3.0.0")
 set_project("hku_rest")
 
-set_version("1.1.3", {build="%Y%m%d%H%M"})
+set_version("1.1.5", {build="%Y%m%d%H%M"})
 
 set_warnings("all")
 set_languages("c++20")
